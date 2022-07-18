32be2803f7cceccff82fdcccdf9e88d785b57ceb
Yujin Hong <hhhyj1077@naver.com>
2022-07-18 16:26:05 +0900

    commit

---
+++ src/main/java/com/kosta/uyeonhi/UyeonhiApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.uyeonhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UyeonhiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UyeonhiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosta/uyeonhi/UyeonhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.uyeonhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UyeonhiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
