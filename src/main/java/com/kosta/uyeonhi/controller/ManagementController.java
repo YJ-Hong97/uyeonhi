7f8b0655d2ff93e7670f4baefc7d62d299eeedc0
Min <Min@MiniNote>
2022-07-19 18:01:00 +0900

    min 0719

--- src/main/java/com/kosta/uyeonhi/controller/ManagementController.java
+++ src/main/java/com/kosta/uyeonhi/controller/ManagementController.java
@@ -2,7 +2,7 @@ package com.kosta.uyeonhi.controller;
  
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController  
+@RestController   
 public class ManagementController {
 
 	
