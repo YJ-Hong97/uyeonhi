0855d2d12bfcd3e69d4346248e7e0a163eea349d
Min <Min@MiniNote>
2022-07-22 17:19:29 +0900

    0722 min

---
+++ src/main/resources/static/js/server.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+var WebSocketServer = require('ws').Server;
+var ws = new WebSocketServer({ port: 3000 });
+
+ws.on('listening', function () {
+	console.log("Server started with port 3000");
+});
+
+ws.on('connection', function (connection) {
+
+    console.log("User is connected");
+    connection.send("Hello from server");
+
+    /* Action to do when user send messages */
+    connection.on('message', function (message) {
+	
+
+        console.log("Message from user....."+message);
+        
+    });
+
+
+    /* Action to do When user try to close the connection */
+    connection.on('close', function () {
+        console.log("Disconnecting user"); 
+    });  
+
+
+}); 
