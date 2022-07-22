8f0c9dc7e5cd0baad43a0c61d5c62e75f898086e
seeun <tpdms1945@gmail.com>
2022-07-22 17:26:04 +0900

    commit

--- src/main/java/com/kosta/uyeonhi/payment/UyeonhiPayController.java
+++ src/main/java/com/kosta/uyeonhi/payment/UyeonhiPayController.java
@@ -1,11 +1,21 @@
 package com.kosta.uyeonhi.payment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.java.Log;
+
+@Log
 @Controller
 @RequestMapping("/uyeonhi/*")
 public class UyeonhiPayController {
@@ -35,7 +45,30 @@ public class UyeonhiPayController {
 	}
 	
 	@PostMapping("/payInsert.go")
-	public void payInsert() {
-		System.out.println("hello");
+	@ResponseBody
+	public String payInsert(@RequestBody Map<String, Object> map) throws ParseException {
+		Map<String, Object> user = (Map<String, Object>) map.get("params");
+		String userId= (String) map.get("userId");//사용자 id
+		int amount=(int) map.get("price");//구매금액
+		int uNum = amount/100;//우연 갯수
+		
+		String date = (String) map.get("requested_at");//구매일자
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date rdate = formatter.parse(date);
+		String rdate2 = formatter.format(rdate);
+		java.sql.Date regdate = java.sql.Date.valueOf(rdate2);
+		
+		System.out.println(user);
+		System.out.println(map);
+		
+		PayVO vo = new PayVO();
+		vo.setId(userId);
+		vo.setUNum(uNum);
+		vo.setRegdate(regdate);
+		vo.setAmount(amount);
+		
+		payRepo.save(vo);
+		return "succes";
 	}
+	
 }
