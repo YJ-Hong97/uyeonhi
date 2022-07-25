c1fb4745dacb166302ee9d8b912c3c2463981c3c
seeun <tpdms1945@gmail.com>
2022-07-25 17:23:46 +0900

    commit

--- src/main/java/com/kosta/uyeonhi/payment/PayRepository.java
+++ src/main/java/com/kosta/uyeonhi/payment/PayRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.kosta.uyeonhi.signUp.UserVO;
+
 
 public interface PayRepository extends CrudRepository<PayVO, Long>{
-	
+	public List<PayVO> findByUser(UserVO user);
 }
--- src/main/java/com/kosta/uyeonhi/payment/PayVO.java
+++ src/main/java/com/kosta/uyeonhi/payment/PayVO.java
@@ -2,14 +2,17 @@ package com.kosta.uyeonhi.payment;
 
 import java.sql.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.kosta.uyeonhi.signUp.UserVO;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,10 +33,12 @@ public class PayVO {
 	@GeneratedValue( strategy =GenerationType.AUTO)
 	private Long  p_id;
 	
-	@Column(length = 255)
-	private String id;
+	@ManyToOne
+	private UserVO user;
 	
-	private int uNum;
+	private int unum;
+	
+	private int current_unum;
 	
 	@CreationTimestamp
 	private Date regdate;
