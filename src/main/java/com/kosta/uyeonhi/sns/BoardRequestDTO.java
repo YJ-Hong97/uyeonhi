d9742838bf59d6c2c015a047c8b0e48bc6de2f67
유승민 <ijbmsm@naver.com>
2022-07-29 17:16:01 +0900

    .

--- src/main/java/com/kosta/uyeonhi/sns/BoardRequestDTO.java
+++ src/main/java/com/kosta/uyeonhi/sns/BoardRequestDTO.java
@@ -3,6 +3,7 @@ package com.kosta.uyeonhi.sns;
 import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -18,40 +19,46 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import com.kosta.uyeonhi.reply.Reply;
+import com.kosta.uyeonhi.signUp.UserRepository;
 import com.kosta.uyeonhi.signUp.UserVO;
 
+import groovy.transform.ToString;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
+@ToString
 public class BoardRequestDTO {
 
 	private Long board_id;
 
 	private String content;
-	
+
 	private String image_path;
-	
+
 	private String video_path;
-	
+
 	private LocalDateTime regdate;
-	
+
 	private LocalDateTime updateDate;
-	
+
 	private UserVO writer;
-	
+
 	private List<Reply> reply;
-	
+
 	private String board_type;
-	
+
 	private int total_person;
-	
+
 	private int applicant_person;
-	
+
 	private Date deadline;
-	
+
 	@Builder
 	public BoardRequestDTO(Long board_id, String content, UserVO writer, String board_type) {
 		this.board_id = board_id;
@@ -59,11 +66,13 @@ public class BoardRequestDTO {
 		this.writer = writer;
 		this.board_type = board_type;
 	}
-	
-	public Board toEntity(){
+
+	public Board toEntity() {
 		return Board.builder()
 				.boardId(board_id)
 				.content(content)
+				.board_type(board_type)
+				.writer(writer)
 				.build();
 	}
 
