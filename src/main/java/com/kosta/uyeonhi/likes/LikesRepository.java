1988fa9a7177c4f133b5b5617b9389072b996c4b
kimjjhh <kim05134@naver>
2022-07-29 17:16:34 +0900

    좋아요 기능 추가

--- src/main/java/com/kosta/uyeonhi/likes/LikesRepository.java
+++ src/main/java/com/kosta/uyeonhi/likes/LikesRepository.java
@@ -1,17 +1,36 @@
 package com.kosta.uyeonhi.likes;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface LikesRepository extends JpaRepository<Likes, Long>{
+public interface LikesRepository extends JpaRepository<Likes, Long> {
 
 	@Modifying
-	@Query(value = "insert into likes (board_board_id, user_id) values (:boardId,:userId)", nativeQuery = true)
-	void likes(long boardId, String userId);
-	
+	@Query(value = "insert into likes (board_board_id,user_id) values (?1,?2)", nativeQuery = true)
+	void likes(Long board_id , String userId );
+
 	@Modifying
-	@Query(value = "DELETE FROM likes WHERE board_board_id = :boardId AND user_id = :userId;", nativeQuery = true)
-	void notLikes(long boardId, String userId);
+	@Query(value = "DELETE FROM likes WHERE board_board_id = ?1 and user_id =?2", nativeQuery = true)
+	void notLikes(Long board_id , String userId );
+
 	
+	/*
+	 * @Modifying
+	 * 
+	 * @Query(value =
+	 * "insert into likes (board_board_id, user_id) values (:board_id,:userId)",
+	 * nativeQuery = true) void likes(Long board_id , String userId );
+	 */
+
+	/*
+	 * @Modifying
+	 * 
+	 * @Query(value =
+	 * "DELETE FROM likes WHERE board_board_id = :board_id AND user_id = :userId;",
+	 * nativeQuery = true) void notLikes(Long board_id , String userId );
+	 */
+
 }
