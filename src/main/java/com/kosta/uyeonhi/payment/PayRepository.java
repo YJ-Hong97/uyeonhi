6ee5eb3850baf53f58929f186cc98281cb6a4e35
seeun <tpdms1945@gmail.com>
2022-08-04 17:34:01 +0900

    commit

--- src/main/java/com/kosta/uyeonhi/payment/PayRepository.java
+++ src/main/java/com/kosta/uyeonhi/payment/PayRepository.java
@@ -1,7 +1,7 @@
 package com.kosta.uyeonhi.payment;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,16 +10,16 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 
 public interface PayRepository extends CrudRepository<PayVO, Long>, QuerydslPredicateExecutor<PayVO> {
-	public List<PayVO> findByUser(UserVO user);
+	public Page<PayVO> findByUser(UserVO user, Pageable paging);
 
 	public default Predicate makePredicate(String type, String keyword) {
 		BooleanBuilder builder = new BooleanBuilder();
 		QPayVO pay = QPayVO.payVO;
+		builder.and(pay.p_id.gt(0));
 
 		if (type == null)
 			return builder;
-		builder.and(pay.p_id.gt(0));
-		
+
 		return builder;
 	}
 }
