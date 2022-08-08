df814bcfa86d7707d4a33aef0b3dd8aa1d5e35cd
Min <Min@MiniNote>
2022-08-08 11:11:48 +0900

    0808

--- src/main/java/com/kosta/uyeonhi/matching/MatchingController.java
+++ src/main/java/com/kosta/uyeonhi/matching/MatchingController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kosta.uyeonhi.signUp.UserVO;
 
@@ -13,13 +15,25 @@ public class MatchingController {
 
 	@Autowired
 	MatchingRepository mRepo;
-	
-	@PostMapping(value = "")
-	public void matchingRequest(String m_id , HttpSession session) {
+
+	@GetMapping(value = "/matching")
+	@ResponseBody
+	public String matchingRequest(String target, HttpSession session) {
 		UserVO user = (UserVO) session.getAttribute("user");
-		MatchingVO vo = new MatchingVO();
-		/*
-		 * vo.setId(session.getAttribute("user")); vo.getTarget(user); vo.setId(user);
-		 */
+		System.out.println(user);
+		MatchingVO vo = MatchingVO.builder()
+				.id(user.getId())
+				.target(target)
+				.m_confirm(0)
+				.build();
+		System.out.println(vo);
+		MatchingVO saveResult = mRepo.save(vo);
+		return saveResult.getM_id() + "OK";
+	}
+	
+	@GetMapping(value = "/matching/test")
+	public String aa() {
+		
+		return "/matching/NewFile";
 	}
 }
--- src/main/java/com/kosta/uyeonhi/matching/MatchingRepository.java
+++ src/main/java/com/kosta/uyeonhi/matching/MatchingRepository.java
@@ -1,14 +1,7 @@
 package com.kosta.uyeonhi.matching;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MatchingRepository extends CrudRepository<MatchingVO, Long>{
+public interface MatchingRepository extends CrudRepository<MatchingVO, Long> {
 
-	@Query(value = "insert into matching (m_id , id , target) values()", nativeQuery = true)
-	void matching(Long m_id , String userId );
 }
