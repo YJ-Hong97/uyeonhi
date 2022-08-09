3d242f476636a7a887689c69fd6481228bd03ea8
kimjjhh <kim05134@naver>
2022-08-09 15:30:05 +0900

    Merge branch 'master' of https://github.com/YJ-Hong97/uyeonhi.git into hyun

--- src/main/java/com/kosta/uyeonhi/applicant/AutoController.java
+++ src/main/java/com/kosta/uyeonhi/applicant/AutoController.java
@@ -2,26 +2,36 @@ package com.kosta.uyeonhi.applicant;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.core.Local;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
+import com.kosta.uyeonhi.likes.Likes;
+import com.kosta.uyeonhi.payment.PayVO;
 import com.kosta.uyeonhi.reply.ReplyService;
 import com.kosta.uyeonhi.signUp.ProfileRepository;
+import com.kosta.uyeonhi.signUp.UserVO;
 import com.kosta.uyeonhi.sns.Board;
 import com.kosta.uyeonhi.sns.BoardRepository;
 import com.kosta.uyeonhi.sns.BoardService;
+import com.kosta.uyeonhi.util.PageMaker;
+import com.kosta.uyeonhi.util.PageVO;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -54,5 +64,7 @@ public class AutoController {
 		
 	
 	}
+	
+	
 
 }
