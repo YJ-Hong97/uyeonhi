7f5f80d4dc47a9a0ce90902667fffe51d9effb32
유승민 <ijbmsm@naver.com>
2022-08-10 03:06:33 +0900

    Merge branch 'master' of https://github.com/YJ-Hong97/uyeonhi.git into Sungmin

--- src/main/java/com/kosta/uyeonhi/sns/BoardApiController.java
+++ src/main/java/com/kosta/uyeonhi/sns/BoardApiController.java
@@ -1,6 +1,8 @@
 package com.kosta.uyeonhi.sns;
 
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +14,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kosta.uyeonhi.signUp.UserRepository;
 import com.kosta.uyeonhi.signUp.UserVO;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 public class BoardApiController {
-	@Autowired
-	private  BoardRepository boardRepository;
 	
-	@Autowired
-	private UserRepository userRepository;
+	private final BoardRepository boardRepository;
 	
-	@Autowired
-	private  BoardService boardService;
+	private final UserRepository userRepository;
+	
+	private final BoardService boardService;
+
+	private final BoardS3Uploader boardS3Uploader;
 	
 	@PostMapping("/api/sns/boardWrite")
-	public long boardWritePost(@RequestBody BoardRequestDTO boardDTO, HttpSession session) {
+	public long boardWritePost(@RequestPart(value = "key") BoardRequestDTO boardDTO, 
+			@RequestPart(value="file") MultipartFile[] files ,HttpSession session) throws IOException {
         UserVO user = (UserVO)session.getAttribute("user");
+        String imageURL = "";
+        for(int i = 0; i < files.length; i++) {
+        	imageURL += boardS3Uploader.upload(files[i], "board");
+        	imageURL += ",";
+        }
+        boardDTO.setImage_path(imageURL);
         boardDTO.setWriter(user);
         return boardService.saveBoard(boardDTO);
 	}
