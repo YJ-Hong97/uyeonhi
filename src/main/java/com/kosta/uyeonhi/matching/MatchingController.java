865e22b0bf63e35d04ecd1032f898faf4846f4c9
Min <Min@MiniNote>
2022-08-10 22:04:28 +0900

    0810 min

--- src/main/java/com/kosta/uyeonhi/matching/MatchingController.java
+++ src/main/java/com/kosta/uyeonhi/matching/MatchingController.java
@@ -21,21 +21,17 @@ public class MatchingController {
 	MatchingRepository mRepo;
 	@Autowired
 	UserRepository uRepo;
-	
+
 	@GetMapping(value = "/matching")
 	@ResponseBody
 	public String matchingRequest(String target, HttpSession session) {
 		UserVO user = (UserVO) session.getAttribute("user");
 		System.out.println(user);
-		
+
 		MatchingVO mats = mRepo.matcheck(user.getId(), target);
-		
-		if(mats == null) {
-			MatchingVO vo = MatchingVO.builder()
-					.id(user)
-					.target(uRepo.findById(target).get())
-					.mconfirm(0)
-					.build();
+
+		if (mats == null) {
+			MatchingVO vo = MatchingVO.builder().id(user).target(uRepo.findById(target).get()).mconfirm(0).build();
 			System.out.println(vo);
 			MatchingVO saveResult = mRepo.save(vo);
 		} else {
@@ -44,8 +40,7 @@ public class MatchingController {
 				mRepo.save(mat);
 			});
 		}
-		
-		
+
 		return "매칭 신청이 되었습니다." + "상대방의 응답을 기다려주세요.";
 	}
 
@@ -69,69 +64,65 @@ public class MatchingController {
 	 */
 
 	@GetMapping(value = "/matView")
+
 	@ResponseBody
-	public List<MatchingVO> matchingResponse(HttpSession session) {
+	public ModelAndView matchingResponse(HttpSession session, ModelAndView mv) {
 		UserVO user = (UserVO) session.getAttribute("user");
 
-		List<MatchingVO> pickMeList = mRepo.findByTargetAndMconfirm(user, 0);
-		//mv.addObject("pickMeList", pickMeList);
-		//mv.setViewName("/matching/matView");
+		List<MatchingVO> pickMeList = mRepo.findByTargetAndMconfirm(user, 0); //
+		mv.addObject("pickMeList", pickMeList); //
+		mv.setViewName("/matching/matView");
 
-		
 		System.out.println(pickMeList.size() + "왔냐고오오오오");
-		System.out.println(pickMeList.get(0) + "오라고오오");
-		
-		/*
-		 * mv.addObject("pickMeList", pickMeList); mv.setViewName("/matching/matView");
-		 */
-		
-		return pickMeList;
+		//System.out.println(pickMeList.get(0) + "오라고오오");
+
+		mv.addObject("pickMeList", pickMeList);
+		mv.setViewName("/matching/matView");
+			
+		return mv;
 	}
-	
+
 	/*
 	 * @GetMapping(value = "/matView")
 	 * 
-	 * @ResponseBody public ModelAndView matchingResponse(HttpSession session ,
-	 * ModelAndView mv) { UserVO user = (UserVO) session.getAttribute("user");
-	 * List<MatchingVO> pickMeList = mRepo.findByTargetAndMconfirm(user.getId(), 0);
+	 * @ResponseBody public MatchingVO matcheck(HttpSession session ,String target)
+	 * { UserVO user = (UserVO) session.getAttribute("user"); MatchingVO list =
+	 * mRepo.matcheck(user.getId(),target); System.out.println(list.toString() +
+	 * "왔냐고오오오오"); //System.out.println(pickMeList.get(0) + "오라고오오");
 	 * 
-	 * System.out.println(pickMeList.size() + "왔냐고오오오오");
-	 * System.out.println(pickMeList.get(0) + "오라고오오");
+	 * //mv.addObject("pickMeList", pickMeList);
+	 * //mv.setViewName("/matching/matView");
 	 * 
-	 * mv.addObject("pickMeList", pickMeList); mv.setViewName("/matching/matView");
-	 * 
-	 * return mv; }
+	 * return list; }
 	 */
-	
+
 	@Transactional
 	@GetMapping(value = "/matYes")
-	public String matchingYes( String pickid  , HttpSession session) {
-		
+	public String matchingYes(String pickid, HttpSession session) {
+
 		pickid = pickid.replaceAll("\"", "");
-		
+
 		UserVO user = (UserVO) session.getAttribute("user");
-		mRepo.modifyMatching( pickid, user.getId());
+		mRepo.modifyMatching(pickid, user.getId());
 		System.out.println(pickid + "--id:" + user.getId());
-		return "redirect:/myPage/"+user.getId();
+		return "redirect:/myPage/";
 	}
-	
-	
+
 	@Transactional
 	@GetMapping(value = "/matNo")
-	public String matchingNo( String pickid  , HttpSession session) {
+	public String matchingNo(String pickid, HttpSession session) {
 		System.out.println(pickid);
 		pickid = pickid.replaceAll("\"", "");
 		UserVO user = (UserVO) session.getAttribute("user");
-		mRepo.deletMatching(pickid ,user.getId());
-		
-		return "redirect:/myPage/'user.getId'";
+		mRepo.deletMatching(pickid, user.getId());
+
+		return "redirect:/myPage/";
 	}
-	
-	
+
 	/*
 	 * @GetMapping(value = "/matching/response") public String aa() {
-	 *  
+	 * 
 	 * return "/matching/NewFile"; }
 	 */
-		 
+
 }
